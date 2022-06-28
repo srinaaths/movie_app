50064ff9348cf8777ad342df0f5ec35589e8a243
srinaaths <srinaath.s@surveysparrow.com>
2022-06-28 12:56:33 +0530

    created server with port 8080

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http')
+
+const PORT = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/movies' && req.method === 'GET') {
+        getAllMovies(req, res);
+    }
+})
+server.listen(PORT, () => console.log(`listening on port ${PORT}`))
