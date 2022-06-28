5b5411e1311fa59bcb6286328cfb656fbced02f6
srinaaths <srinaath.s@surveysparrow.com>
2022-06-28 13:17:09 +0530

    regex in route for specific id

--- server.js
+++ server.js
@@ -7,5 +7,8 @@ const server = http.createServer((req, res) => {
     if(req.url === '/movies' && req.method === 'GET') {
         getAllMovies(req, res);
     }
+    else if(req.url.match(/\/movies\/([0-9]+)/)) {
+        const id = (req.url.split('/'))[2];
+    }
 })
 server.listen(PORT, () => console.log(`listening on port ${PORT}`))
